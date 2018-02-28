baba4046831432033d2a0cbb6781b4c2a4ca9f7d
Jafar Sarif <jeffstone1994@gmail.com>
2018-02-28 23:10:19 +0530

    Add files via upload

---
+++ core.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+api_key = "d725b1c651074e7fa238041ef5dccb0a"
+url = "https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey="+api_key
+
+
+def print_news(articles,id):
+	print("{id}) {title}\n BY:{by}\tDate:{date}\n{description}\n{url}"
+		.format(id=id,title=str(articles["title"]),by=str(articles["author"]),date=str(articles["publishedAt"])
+			,description=str(articles["description"]),url=str(articles["url"])))
+	print("-------------------------------------------")
+
+def get_news():
+	res_json = requests.get(url).json()
+	i = 1
+	if str(res_json['status']) == 'ok':
+		for articles in res_json["articles"]:
+			print_news(articles,i)
+			i = i + 1
+
+get_news()	
